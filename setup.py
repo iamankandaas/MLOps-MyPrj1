841f188ae495960b4e5d05ae8c80128c5e2be2be
iamankandaas <das.99.ankan@gmail.com>
2025-07-19 10:45:14 +0530

    project structure ready, venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Ankan Das",
+    author_email="das.99.ankan@gmail.com",
+    packages=find_packages()
+)
